cdfbb36733c3b5983e31b85e763f6d94c5e800ec
liuxb <liuxb@hx168.com.cn>
2017-10-09 17:18:40 +0800

    view-koa

--- controllers/hello.js
+++ controllers/hello.js
@@ -1,9 +1,16 @@
 'use strict';
 
+// var fn_hello = async(ctx, next) => {
+//     var name = ctx.params.name;
+//     ctx.response.type = 'text/html';
+//     ctx.response.body = `<h1>hello ${name}</h1>`;
+// };
+
 var fn_hello = async(ctx, next) => {
     var name = ctx.params.name;
-    ctx.response.type = 'text/html';
-    ctx.response.body = `<h1>hello ${name}</h1>`;
+    ctx.render('hello.html', {
+        title: `welcome ${name}`
+    });
 };
 
 module.exports = {
--- controllers/index.js
+++ controllers/index.js
@@ -1,24 +1,48 @@
 'use strict';
 
+// var fn_index = async(ctx, next) => {
+//     ctx.response.type = 'text/html';
+//     ctx.response.body = `<h1>Index</h1>
+//     <form action="/signin" method="post">
+//         <p>Name: <input name="name" value="koa"></p>
+//         <p>Password: <input name="password" type="password"></p>
+//         <p><input type="submit" value="Submit"></p>
+//     </form>`;
+// };
+
+// var fn_signin = async(ctx, next) => {
+//     var name = ctx.request.body.name || '',
+//         password = ctx.request.body.password || '';
+//     console.log(`name: ${name}, password: ${password}`);
+//     if (name === 'koa' && password === '123456') {
+//         ctx.response.body = `<h1>hello ${name}</h1>`;
+//     } else {
+//         ctx.response.body = `<h1>login failed!</h1>
+//         <p><a href='/'>try again</a></p>`;
+//     }
+// };
+
 var fn_index = async(ctx, next) => {
-    ctx.response.type = 'text/html';
-    ctx.response.body = `<h1>Index</h1>
-    <form action="/signin" method="post">
-        <p>Name: <input name="name" value="koa"></p>
-        <p>Password: <input name="password" type="password"></p>
-        <p><input type="submit" value="Submit"></p>
-    </form>`;
+    ctx.render('index.html', {
+        title: 'Welcome'
+    });
 };
 
 var fn_signin = async(ctx, next) => {
-    var name = ctx.request.body.name || '',
+    var
+        email = ctx.request.body.email || '',
         password = ctx.request.body.password || '';
-    console.log(`name: ${name}, password: ${password}`);
-    if (name === 'koa' && password === '123456') {
-        ctx.response.body = `<h1>hello ${name}</h1>`;
+    if (email === 'admin@example.com' && password === '123456') {
+        // 登录成功:
+        ctx.render('signin-ok.html', {
+            title: 'Sign In OK',
+            name: 'Mr Node'
+        });
     } else {
-        ctx.response.body = `<h1>login failed!</h1>
-        <p><a href='/'>try again</a></p>`;
+        // 登录失败:
+        ctx.render('signin-failed.html', {
+            title: 'Sign In Failed'
+        });
     }
 };
 
